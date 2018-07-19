ef406704788c90e44c03414fae20d80209992b26
Deepika Mohan <deepikamm12@gmail.com>
2018-07-19 10:54:50 +0530

    updated

--- RosterManagement/Roster.cs
+++ RosterManagement/Roster.cs
@@ -12,14 +12,32 @@ namespace RosterManagement
         {
             _roster = new Dictionary<int, List<String>>();
         }
-        
+
         /// <summary>
         /// Should be able to Add Student to a Particular Wave
         /// </summary>
         /// <param name="cadet">Refers to the name of the Cadet</param>
         /// <param name="wave">Refers to the Wave number</param>
         public void Add(string cadet, int wave)
+
         {
+            
+           try
+            {
+               
+                _roster[wave].Add(cadet);
+                //_roster[wave].Sort();
+               
+            }
+            catch(Exception e)
+            {
+                List<string> list = new List<string>();
+                list.Add(cadet);
+                _roster.Add(wave, list);
+                
+
+            }
+
         }
 
         /// <summary>
@@ -29,8 +47,16 @@ namespace RosterManagement
         /// <returns>List of Cadet's Name</returns>
         public List<string> Grade(int wave)
         {
-            var list = new List<string>();
-            return list;
+            List<string> list=new List<string>();
+            if (_roster.ContainsKey(wave))
+            {
+                 list = _roster[wave];
+            }
+            var orderByDescendingResult = from c in list
+                                          orderby c
+                                          select c;
+            List<string> list1 = new List<string>(orderByDescendingResult);
+            return list1;
         }
 
         /// <summary>
@@ -40,7 +66,16 @@ namespace RosterManagement
         public List<string> Roster()
         {
             var cadets = new List<string>();
+            SortedDictionary<int, List<string>> roster = new SortedDictionary<int, List<string>>(_roster);
+            foreach (List<string> v in roster.Values)
+            {
+                v.Sort();
+                cadets.AddRange(v);
+               
+            }
+           
             return cadets;
+            
         }
     }
 }
